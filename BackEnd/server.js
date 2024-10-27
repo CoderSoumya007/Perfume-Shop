752483ec5e7ae0b42a0ec70b48510fec29d1969b
codersoumya007 <soumyapanda87631@gmail.com>
2024-10-27 16:44:51 +0530

    home router

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,7 +11,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors());    
+
+app.use("/",(req,res)=>{
+    res.send("Hello User")
+    console.log("Server is running");
+})
 
 connectToDatabase();
 
